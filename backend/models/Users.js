ed38a9e50f0c04fbace48c136ea92cdfde954c8d
Satyam Gupta <satya.supercluster@gmail.com>
2023-11-12 18:07:29 +0530

    saving login information of admin on mongodb

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema(
         role:{
             type:String,
             required:true
-        }
+        },
     },
     { timestamps: true}
 );
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,19 +1,80 @@
 const admin =require('../config/firebase.config');
+const users = require('../models/Users');
 const router=require('express').Router();
+
+
+// Function for making new admin
+const newUserData = async (decodedValue,req,res)=>{
+
+    const newUser = new users({
+
+        name:decodedValue.name,
+        email:decodedValue.email,
+        imageUrl:decodedValue.picture,
+        userId:decodedValue.user_id,
+        email_verified:decodedValue.email_verified,
+        role:"admin"
+
+    });
+
+    //  Saving the data
+    
+        try{
+            const savedUser = await newIser.save();
+            res.status(200).send({user: savedUser});
+        }
+        catch(error){
+            res.status(400).send({success:false,msg: error});
+        }
+
+        
+};
+
+
+
 
 // Logging in Admin
 
 router.post("/admin/login",async (req,res)=>{
+    
+
+    // Checking if firebase has sent userCredentials
+
+
     if(!req.headers.authorization){
         return res.status(500).send({message:"Something went wrong"});
     }
     const token =req.headers.authorization.split(" ")[1];
     try{
+
+        // Decoding the data from token sent by firebase as userCredentials
+
+
         const decodedValue= await admin.auth().verifyIdToken(token);
         if(!decodedValue){
             return res.status(500)
             .json({success:false,message:"Unauthorized User"});
         }
+        console.log(decodedValue);
+        // Checking if the admin already exists or not
+
+        const userExists = await users.findOne({userId: decodedValue.user_id});
+
+
+        if(!userExists){
+            
+            // Admin Do not exsits
+            // So
+            // Make new Admin
+
+            newUserData(decodedValue,req,res);
+
+        }
+        else{
+            res.send("No problem");
+        }
+
+
     }
     catch(error){
         return res.status({success:false,msg:error})
@@ -40,9 +101,14 @@ router.post("/student/login",async (req,res)=>{
 
 
 
+
+
+
 router.get('/',(req,res)=>{
     console.log("hellp");
     res.send("hello");
 });
+
+
 
 module.exports=router;
