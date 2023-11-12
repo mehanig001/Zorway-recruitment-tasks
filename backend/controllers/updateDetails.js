2cee2665bf1acbfb00fd02525c7b16d2b397af92
Satyam Gupta <satya.supercluster@gmail.com>
2023-11-12 22:14:30 +0530

    student can update their own details

--- backend/controllers/updateDetails.js
+++ backend/controllers/updateDetails.js
@@ -1,18 +1,40 @@
 const users = require('../models/Users');
 
 const updateDetails = async (req,res)=>{
+
+    // req.body contains the id of current and userid of the user which is to be updated
+
+
     const {id,userid,...rest} =req.body;
+
+    // Finding current user
+
     const current=await users.findOne({userId:id}).exec();
+
     if(!current){
         return res.status(500).json({success:false,msg:"Invalid User Id"});
     }
+
+    // Finding target user
+
     const target=await users.findOne({userId:userid}).exec();
     if(!target){
         return res.status(500).json({success:false,msg:"Invalid Target User Id"});
     }
+
+    // students can only update their own details nothing else
+
     if(current.role==="student"){
         if(id===userid){
-            return res.send("update");
+
+            try{
+                const modified= await users.findOneAndUpdate({userId:userid},rest,{new:true});
+                res.json(modified);
+            }
+            catch(e){
+                req.status(500).json({error:"Something went wrong while updating the data"});
+            }
+            
         }
         else{
             return res.status(500).json({success:false,msg:"Student cannot change details of others"});
