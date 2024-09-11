a77db847116704832e08abeb2355f73933f85677
mehanig001 <gautammihani258@gmail.com>
2024-09-11 23:22:35 +0530

    satyam

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const {default:mongoose}=require('mongoose');
 const dotenv=require('dotenv');
-const route = require('./routes/routes');
+// const route = require('./routes/routes');
 dotenv.config();
 const morgan =require("morgan");
 const bodyParser = require('body-parser');
@@ -10,12 +10,6 @@ const cors =require('cors');
 
 const app=express();
 
-// Checking the server
-app.get('/',(req,res)=>{
-    res.send("<H1>Hey There, I'm Here</H1>")
-})
-
-app.use(cors());
 
 // middlewares
 app.use(morgan("tiny"));
@@ -23,20 +17,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
+// Checking the server
+app.get('/',(req,res)=>{
+    res.send("<H1>Hey There, I'm Here</H1>")
+})
+app.post('/',(req,res)=>{
+    res.send("<H1>Hey There, I'm juii</H1>")
+})
+app.get('/hello/:name/:age',(req,res,next)=>{next();},(req,res,next)=>{next();},(req,res)=>{
+    const {name}=req.params;
+    res.status(200).json({success:true,data:`<H1>Hey There, I'm ${name}</H1>`})
+})
+
+app.use(cors());
+
+
+
+
 //Routers
-app.use("/api/users",route);
+// app.use("/api/users",route);
 
 
 
 //Mongoose Connection
-mongoose
-.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("MONGOOSE CONNECTED");
-})
-.catch(()=>{
-    console.log("Not Connencted to MONGOOSE");
-})
+// mongoose
+// .connect(process.env.MONGO_URL)
+// .then(()=>{
+//     console.log("MONGOOSE CONNECTED");
+// })
+// .catch(()=>{
+//     console.log("Not Connencted to MONGOOSE");
+// })
 
 
 // Listening the server
